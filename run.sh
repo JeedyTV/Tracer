0aef002d408f6512a37c230d4c34199c7118fbad
JeedyTV <jdmukolonga@student.uliege.be>
2021-03-29 22:48:39 +0200

    change the run to ch

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-gcc test.c --static -o out
+
+
 gcc test.S -g -o tracee --static -nostdlib -Wl,-entry="main"
+
+echo "compile et run backup.c"
+gcc backup.c --static -o out
+./out tracee
+echo "---compile main.c marche pas"
+gcc main.c --static -o out
+echo "---run main.c marche pas"
 ./out tracee
