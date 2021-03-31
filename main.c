fa5788f85d372f60ecbd8ba7bb089ecb73682036
Christal <christal.mango@gmail.com>
2021-03-31 21:58:17 +0200

    fusion

--- main.c
+++ main.c
@@ -18,52 +18,87 @@
 #include <sys/reg.h> /* EAX, ORIG_EAX */
 #include <sys/user.h>
 #include "sys_call.h"
+#include "stack.h"
 
 
-int wait_(pid_t pid) {
-    int status;
-    waitpid(pid, &status, 0);
-    if(WIFSTOPPED(status)) return 0;
-    else if (WIFEXITED(status)) return 1;
-    else {
-        printf("%d status should be STOP or EXIT\n", pid);
-        return 1;
+unsigned long get_start(char * tracee_name){
+
+    char command[DEST_SIZE]="readelf --segments ";
+
+    strcat(command, tracee_name);
+
+    char * command2 = " | grep 'Entry point' > entry";
+
+    strcat(command, command2);
+
+    int cr = system(command);
+    if ( cr != 0 ){
+        fprintf(stderr, "Impossible de lancer la commande : readelf\n");
+        return 0;
     }
+
+    char trash[DEST_SIZE];
+
+    FILE *fichier = fopen("entry","r");
+
+    unsigned long address;
+
+    fscanf(fichier,"%s %s 0x%lx",trash,trash,&address);
+
+    cr = system("rm entry");
+    if ( cr != 0 ){
+        fprintf(stderr, "Impossible de lancer la commande : rm\n");
+        return 0;
+    }
+
+    return address;
 }
 
 bool isCall(unsigned long instruction){
     unsigned int opcode1 = instruction & 0x000000FF;
     return (opcode1 == 0x9A || opcode1 == 0xE8 || opcode1 == 0xFF);
 }
-bool isRet(unsigned long instruction){
-    unsigned int opcode1 = instruction &0x000000FF;
-    return (opcode1 >= 0xC2 && opcode1 <= 0xC3) || (opcode1 >= 0xCA && opcode1 <= 0xCB);
+
+bool isRet(unsigned long adresse,unsigned ret_addresse){
+    
+    return adresse == ret_addresse ;
 }
 
 int start_tracer(pid_t child,char *programname){
-    unsigned long instruction, ip;
+    unsigned long instruction = 0;
+    unsigned long return_address = 0;
+    struct user_regs_struct regs;
     fun_tree *heap = NULL;
-    //fun_tree *heap = new_fun_tree(NULL, 0, NULL);
-
+   
     size_t nb_instr = 0;
     size_t nb_calls = 0;
     size_t nb_returns = 0;
-    bool just_called = 0;
-    bool just_returned = 0;
+    
+    bool just_called = false;
+    bool just_returned = false;
+    Stack *stack = createStack(2);
+    Dic *d = get_labels_dic(programname);
+    unsigned long start = get_start(programname);
+    bool started = false;
     fun_tree *current = heap;
     
-    // Dic *d = get_labels_dic(programname);
-
-    while (wait_(child) < 1) {
-        ip = ptrace(PTRACE_PEEKUSER, child, 4 * EIP, NULL);
-        if(ip < 0){
-            perror("problem ptrace ip");
+    int status = 0;
+  
+    while (waitpid(child,&status,0) && ! WIFEXITED(status)) {
+        if(ptrace(PTRACE_GETREGS, child, NULL, &regs) < 0){
+            perror("problem regs");
             return 1;
         }
-        instruction = ptrace(PTRACE_PEEKTEXT, child, ip, NULL);
+
+        instruction = ptrace(PTRACE_PEEKTEXT, child, regs.eip, NULL);
         if(instruction < 0){
-            perror("problem ptrace instruction");
-            return 1;
+                perror("problem ptrace instruction");
+                return 1;
+        }
+        return_address = ptrace(PTRACE_PEEKTEXT, child, regs.esp, NULL);
+        if(return_address < 0){
+                perror("problem ptrace instruction");
+                return 1;
         }
 
         if(ptrace(PTRACE_SINGLESTEP, child, NULL, NULL) < 0){
@@ -71,75 +106,82 @@ int start_tracer(pid_t child,char *programname){
             return 1;
         }
         
-        // fun_tree *father;   // The function that calls
-        // fun_tree *son;      // The function that is called
-        
-        ++nb_instr;
 
-        if (just_called){
-            if (!heap){
-                printf("premier call:\n");
-                heap = new_fun_tree("le nom de l'instru", 0, NULL);
-                current = heap;
-                nb_instr = 0;
-                just_called = false;
-                ++nb_calls;
-                continue;
-            }
-
-            current->nb_instructions += nb_instr;
-            nb_instr = 0;
-            if(current->label != "le nom de l'instru"){
-                printf("nouveau call:\n");
-                current->next = new_fun_tree("le nom de la nouvelle instru appelée", current->depth +1, current);
-                current = current->next;
-                ++nb_calls;
-            }
-            else{
-                printf("call de récursion:\n");
-                current->recursive = true;
-                current->nb_rec_calls++;
-                current->recursion_stage++;
-            }
-            just_called = false;
+        if(start == regs.eip) {
+            started = true;
+            printf("THe entry point");
         }
-        else if(just_returned && heap){
-            printf("fin de la fonction\n");
-            if(current->recursive && current->recursion_stage){
-                printf("et c'est une récursion\n");
-                current->nb_instructions += nb_instr;
-                nb_instr =0;
-                current->recursion_stage--;
-            }
-            else{
-                printf("et ce n'était pas une récursion\n");
+
+
+        if(started){
+            nb_instr++;
+
+            if (just_called){
+                push(stack,return_address);
+                if (!heap){
+                    printf("premier call:\n");
+                    heap = new_fun_tree("le nom de l'instru", 0, NULL);
+                    current = heap;
+                    nb_instr = 0;
+                    just_called = false;
+                    ++nb_calls;
+                    continue;
+                }
+
                 current->nb_instructions += nb_instr;
                 nb_instr = 0;
-                ++nb_returns;
-                if(current->prev){
-                    current->prev->subtree = current;
-                    current->prev->next = NULL;
-                    current= current->prev;
+                if(current->label != "le nom de l'instru"){
+                    printf("nouveau call:\n");
+                    current->next = new_fun_tree("le nom de la nouvelle instru appelée", current->depth +1, current);
+                    current = current->next;
+                    ++nb_calls;
                 }
                 else{
-                    printf("dernière function");
-                    //print_tree(current);
-                    //delete_fun_tree(current);
-                    break;
+                    printf("call de récursion:\n");
+                    current->recursive = true;
+                    current->nb_rec_calls++;
+                    current->recursion_stage++;
                 }
+                just_called = false;
             }
-            just_returned = false;
+            else if(just_returned && heap){
+                printf("fin de la fonction\n");
+                if(current->recursive && current->recursion_stage){
+                    printf("et c'est une récursion\n");
+                    current->nb_instructions += nb_instr;
+                    nb_instr =0;
+                    current->recursion_stage--;
+                }
+                else{
+                    printf("et ce n'était pas une récursion\n");
+                    current->nb_instructions += nb_instr;
+                    nb_instr = 0;
+                    ++nb_returns;
+                    if(current->prev){
+                        current->prev->subtree = current;
+                        current->prev->next = NULL;
+                        current= current->prev;
+                    }
+                    else{
+                        printf("dernière function");
+                        print_tree(current);
+                        delete_fun_tree(current);
+                        break;
+                    }
+                }
+                just_returned = false;
+            }
+            if (isCall(instruction)) {
+                printf("called: %s\n",get_label(d,regs.eip));
+                just_called = true;
+            }
+            else if(isRet(regs.eip,peak(stack))){
+                printf("Y a un return\n");
+                pop(stack);
+                just_returned = true;
+            }
+            printf("--%08lx--,--%08lx--\tcall:%d,ret:%d\n",instruction,ip,nb_calls,nb_returns);
         }
-        if (isCall(instruction)) {
-            printf("Y a un call\n");
-            just_called = true;
-        }
-        else if (isRet(instruction)){
-            printf("Y a un return\n");
-            just_returned = true;
-        }
-        printf("--%08lx--,--%08lx--\tcall:%d,ret:%d\n",instruction,ip,nb_calls,nb_returns);
-
         // do
         // {
         //     printf("--%08lx--,--%08lx--\tcall:%d,ret:%d\n",instruction,ip,isCall(instruction),isRet(instruction));
@@ -167,7 +209,8 @@ int start_tracer(pid_t child,char *programname){
     //     current = current->next;
     //     delete_fun_tree(prec);
     // }
-
+    free_stack(stack);
+    free_dic(d);
     return 0;
 }
 
